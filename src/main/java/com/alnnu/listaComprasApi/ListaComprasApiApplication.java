f85f3aed4051bc57e01c687253e934ef8f705833
alnnu <luannf1020@gmail.com>
2026-04-21 10:57:56 -0300

    init

---
+++ src/main/java/com/alnnu/listaComprasApi/ListaComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.alnnu.listaComprasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaComprasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnnu/listaComprasApi/ListaComprasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnnu.listaComprasApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaComprasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

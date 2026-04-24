933e9178387683198a500064529b6b9c60c3a77f
alnnu <luannf1020@gmail.com>
2026-04-24 17:48:49 -0300

    add: tests for create list and add one product

--- src/main/java/com/alnnu/listaComprasApi/ListaComprasApiApplication.java
+++ src/main/java/com/alnnu/listaComprasApi/ListaComprasApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ListaComprasApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ListaComprasApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ListaComprasApiApplication.class, args);
+  }
 
 }
---
+++ src/test/java/com/alnnu/listaComprasApi/ListServiceTest.java
@@ -0,0 +1,108 @@
+package com.alnnu.listaComprasApi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alnnu.listaComprasApi.entities.ListEntity;
+import com.alnnu.listaComprasApi.entities.ProductEntity;
+import com.alnnu.listaComprasApi.entities.dto.ProductIdsDTO;
+import com.alnnu.listaComprasApi.repositories.ListRepository;
+import com.alnnu.listaComprasApi.repositories.ProductRepository;
+import com.alnnu.listaComprasApi.services.ListService;
+import com.alnnu.listaComprasApi.utils.exceptions.ActiveListException;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+@ExtendWith(MockitoExtension.class)
+public class ListServiceTest {
+
+  @Mock
+  ListRepository repositoryMock;
+
+  @Mock
+  ProductRepository productRepositoryMock;
+
+  @InjectMocks
+  ListService service;
+  /*
+   * Test if the service is creating a list: For this create a list using the
+   * service and assert if value returned is not null and verify the mock call
+   */
+
+  @Test
+  @DisplayName("Create List Test Pass")
+  void createListPass() {
+
+    when(repositoryMock.existsByActiveTrue()).thenReturn(false);
+
+    ListEntity entity = service.createOne();
+
+    assertNotNull(entity, "should exist one list active");
+    verify(repositoryMock, times(1)).existsByActiveTrue();
+  }
+
+  /*
+   * Test if the service emit a ActiveListException if try to create a list with
+   * another list active: For this return true for
+   * repositoryMock.existsByActiveTrue and try to create a list, assert the
+   * exception and verify the mock call
+   */
+  @Test
+  @DisplayName("Create List Test Fail")
+  void createListFail() {
+
+    when(repositoryMock.existsByActiveTrue()).thenReturn(true);
+
+    assertThrows(ActiveListException.class, () -> service.createOne());
+    verify(repositoryMock, times(1)).existsByActiveTrue();
+  }
+
+  /*
+   * Test adding a product to an active list: for this addProducts assertNotNull
+   * for the list, assert the product in the list is equal to product and verify
+   * mocks calls
+   */
+  @Test
+  @DisplayName("Add a single product to an active list")
+  void addProductAtiveList() {
+    List<Long> ids = new ArrayList<Long>();
+
+    ids.add(1l);
+
+    ProductIdsDTO productIdsDTO = new ProductIdsDTO(ids);
+
+    ListEntity list = new ListEntity();
+
+    ProductEntity product = new ProductEntity(1l, "test", "test", "test");
+
+    when(repositoryMock.findByActiveTrue()).thenReturn(Optional.of(list));
+    when(productRepositoryMock.findById(1l)).thenReturn(Optional.of(product));
+
+    list = service.addProducts(productIdsDTO);
+
+    assertNotNull(list);
+    assertEquals(list.getProducts().get(0), product);
+    verify(repositoryMock, times(1)).findByActiveTrue();
+    verify(productRepositoryMock, times(1)).findById(1l);
+  }
+}
